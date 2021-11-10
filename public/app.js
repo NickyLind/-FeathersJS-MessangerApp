6e4cf24c337d550c3d1f0d0e2a4337d492bb6e70
Nick Lindau <nicholaithegreat@gmail.com>
2021-11-09 19:36:50 -0800

    create very basic client-side functionality

---
+++ public/app.js
@@ -0,0 +1,28 @@
+const socket = io();
+
+const client = feathers();
+client.configure(feathers.socketio(socket));
+
+client.configure(feathers.authentication({
+  storage: window.localStorage
+}));
+
+const login = async () => {
+  try {
+    return await client.reAuthenticate();
+  } catch (error) {
+    return await client.authenticate({
+      strategy: 'local',
+      email: 'hello@feathersjs.com',
+      password: 'supersecret'
+    })
+  }
+};
+
+const main = async () => {
+  const auth = await login();
+
+  console.log('user is authenticated!', auth);
+};
+
+main();
